7ce1e0eef9491da11cc91bb880061b11662f956e
Taylor Richmond <taylorichmond@gmail.com>
2015-04-02 02:18:10 -0400

    Initial commit. At this point, the only thing I have created is the search function in helpers.c. Everything else was created by the CS50 staff at Harvard.

---
+++ pset3/find/helpers.c
@@ -0,0 +1,42 @@
+/**
+ * helpers.c
+ *
+ * Computer Science 50
+ * Problem Set 3
+ *
+ * Helper functions for Problem Set 3.
+ */
+       
+#include <cs50.h>
+
+#include "helpers.h"
+
+/**
+ * Returns true if value is in array of n values, else false.
+ */
+bool search(int value, int values[], int n)
+{
+    if (n < 1)
+    {
+        return false;
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        if (value == values[i])
+        {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+/**
+ * Sorts array of n values.
+ */
+void sort(int values[], int n)
+{
+    // TODO: implement an O(n^2) sorting algorithm
+    return;
+}
